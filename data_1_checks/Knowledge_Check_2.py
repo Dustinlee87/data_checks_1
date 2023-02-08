add63a4577987b87ea2d3123be20d9a1c6978243
Dustinlee87 <122577752+Dustinlee87@users.noreply.github.com>
2023-02-08 11:10:11 -0500

    Delete Knowledge_Check_2.py

--- data_1_checks/Knowledge_Check_2.py
+++
@@ -1,5 +0,0 @@
-print("hello world!") 
-
-The_Office_Characters = ["Angela", "Michael", "Andy", "Kevin", "Meredith", "Creed", "Oscar", "Pam", "Jim", "Dwight", "Kelly", "Jan", "David", "Ryan", "Robert", "Darryl", "Erin",
- "Toby", "Holly", "Stanley", "Bob", "Phyllis", "Pete", "Gabe", "Roy", "Karen", "Clark", "Deangelo", "Hank", "Jo", "Todd", "Mose", "Charles", "Nellie"] 
-print(The_Office_Characters[7]) 
